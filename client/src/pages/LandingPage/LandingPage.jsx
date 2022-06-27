c268f549674516644bd08aa7b56a19dafadfdd9f
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-06-27 03:06:41 +0000

    WIP

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 export default function LandingPage()
 {
     return (
-        <div>
+        <div className="center">
             <div className="bg-black text-white text-[20px]">
                 <Marquee gradient={false}>
                     WELCOME! AVAIL OUR BEST PROMOS, UNLIMITED VOUCHERS AND MORE. SO WHAT ARE YOU WAITING FOR. <span className="text-red-500">SHOP NOW!!!!</span>
