28c66f4c236d2dca86f14c8a4dcc4be81c67eb2b
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-06-28 09:39:12 +0000

    WIP

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 
-// import LandingPage from "./pages/LandingPage/LandingPage";
 const LandingPage = React.lazy(() => import('./pages/LandingPage/LandingPage'))
 const About = React.lazy(() => import('./pages/About/About'))
+const Contact = React.lazy(() => import('./pages/Contact/Contact'))
 
 export default function Router()
 {
@@ -14,7 +14,7 @@ export default function Router()
                 <Route path='/' element={<MainLayout />} >
                     <Route index element={<LandingPage />} />
                     <Route path='about' element={<About />} />
-                    <Route path='contact' element={null} />
+                    <Route path='contact' element={<Contact />} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import Banner from "../../components/Banner/Banner";
+
+
+export default function Contact()
+{
+    return (
+        <>
+            <Banner title='CONTACT US' />
+        </>
+    )
+}
