ddcabe276d84f3b8c8b82fdeba273254915ead63
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-07-08 11:48:11 +0000

    Design Create Category Page

--- resources/js/pages/Admin/Categories/CreateCategory.jsx
+++ resources/js/pages/Admin/Categories/CreateCategory.jsx
@@ -1,13 +1,37 @@
+import { FormItem } from "@/components";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Switch } from "antd";
 
 
 export default function CreateCategory() {
     return (
-        <div className="mt-6 bg-white max-w-screen-sm mx-auto shadow-lg rounded">
-            <div className="p-4 border-b border-gray-200">
-                <span className="font-semibold text-lg text-gray-800">Category Information</span>
-            </div>
-            <div className="p-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus est voluptas, totam eum assumenda quam rerum quos amet tempora error dolore non quisquam, cumque distinctio dignissimos quod voluptates? Quis, eum?
+        <div className="sm:px-4">
+            <div className="mt-6 bg-white max-w-screen-sm mx-auto shadow-lg rounded">
+                <div className="p-4 border-b border-gray-200">
+                    <span className="font-semibold text-lg text-gray-800">Category Information</span>
+                </div>
+                <div className="p-4">
+                    <Form
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                    >
+                        <FormItem name='name' label='Product Category Title'>
+                            <Input />
+                        </FormItem>
+                        <FormItem name='image' label='Banner'>
+                            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                        </FormItem>
+                        <Form.Item name='is_featured' label='Featured' valuePropName="checked">
+                            <Switch />
+                        </Form.Item>
+
+                        <FormItem wrapperCol={{ offset: 8, span: 16 }}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </div>
             </div>
         </div>
     )
